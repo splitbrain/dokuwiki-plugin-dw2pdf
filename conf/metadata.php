eea1c90d7798e68e93156d445df399a3e77220eb
Johannes Fürwentsches <johannes@fuerwentsches.de>
2012-01-16 15:51:14 +0100

    QR Code Extension.

--- conf/metadata.php
+++ conf/metadata.php
@@ -4,4 +4,5 @@ $meta['usecache']     = array('onoff');
 $meta['template']     = array('dirchoice', '_dir' => DOKU_PLUGIN.'dw2pdf/tpl/');
 $meta['maxbookmarks'] = array('numeric');
 $meta['usestyles']    = array('string');
-
+$meta['useqrcodes']   = array('onoff');
+$meta['qrcodesize']   = array('string');
