935f0f0c415edcb68cc61d39663e6a51d0b315e6
Andreas Gohr <gohr@cosmocode.de>
2012-01-23 13:52:32 +0100

    fixed syntax error

--- action.php
+++ action.php
@@ -134,7 +134,7 @@ class action_plugin_dw2pdf extends DokuWiki_Action_Plugin {
         header('Pragma: public');
         http_conditionalRequest(filemtime($cache->cache));
 
-        $filename = cleanID(strtr(':/;"','    ',$title))
+        $filename = cleanID(strtr(':/;"','    ',$title));
         if($this->getConf('output') == 'file'){
             header('Content-Disposition: attachment; filename="'.rawurlencode($title).'.pdf";');
         }else{
