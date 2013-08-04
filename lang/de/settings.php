dd47e4acf8b75787951eacee9a748a7adca2f8f6
Sven <Svenluecke48@gmx.de>
2013-08-04 17:00:04 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,15 +1,15 @@
 <?php
+
 /**
- * German language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sven <Svenluecke48@gmx.de>
  */
-
-// fuer die Plugin-Konfiguration
-
-$lang['output']           = 'So wird die Datei ausgegeben';
-$lang['output_o_browser'] = 'Browser';
-$lang['output_o_file']    = 'Datei herunterladen';
-$lang['template']         = 'Welches Template soll zur Formatierung der PDFs verwendet werden?';
-$lang['maxbookmarks']     = 'Bis zu welcher Tiefe werden Lesezeichen angezeigt? (0=keine, 5=alle)';
-
-
+$lang['output']                = 'So wird die Datei ausgegeben';
+$lang['output_o_browser']      = 'Browser';
+$lang['output_o_file']         = 'Datei herunterladen';
+$lang['usecache']              = 'Sollten PDFs zwischengespeichert werden? Eingebettete Bilder werden dann nicht ACL-geprüft. Wenn das ein Sicherheitsbedenken darstellt, sollte diese Option deaktiviert werden.';
+$lang['template']              = 'Welches Template soll zur Formatierung der PDFs verwendet werden?';
+$lang['maxbookmarks']          = 'Bis zu welcher Tiefe werden Lesezeichen angezeigt? (0=keine, 5=alle)';
+$lang['usestyles']             = 'Eine Komma-getrennte Liste für die Plugins wessen <code>style.css</code> oder <code>screen.css</code> für die PDF-Erzeugung genutzt werden soll. Standardmäßig werden die <code>print.css</code> und <code>pdf.css</code> verwendet.';
+$lang['qrcodesize']            = 'Größe von eingebetteten QR-Codes (<code><i>Breite</i><b>x</b><i>Höhe</i></code> in Pixeln). Leer zum deaktivieren';
