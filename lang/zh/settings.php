05cccb323ff7a19a20226db04bfcfcdcc7260b2d
Mofe <mofe@me.com>
2013-10-10 09:50:42 +0200

    translation update

---
+++ lang/zh/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mofe <mofe@me.com>
+ */
+$lang['output']                = '怎样显示PDF文件？';
+$lang['output_o_browser']      = '在浏览器中显示';
+$lang['output_o_file']         = '下载PDF到本地';
+$lang['template']              = '使用哪个模板的格式来排版PDF的内容？';
