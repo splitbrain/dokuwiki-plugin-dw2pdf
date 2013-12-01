3f24b1c26843aaadc068d0159d3a3bf47b8d0250
Juliano Marconi Lanigra <juliano.marconi@gmail.com>
2013-12-01 16:20:05 +0100

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Juliano Marconi Lanigra <juliano.marconi@gmail.com>
+ */
+$lang['output']                = 'Como o PDF deveria ser apresentado ao usuário?';
+$lang['output_o_browser']      = 'Mostrar no navegador';
+$lang['output_o_file']         = 'Fazer o download do PDF';
+$lang['usecache']              = 'Os PDFs deveriam ser armazenados em cache? Imagens incorporadas não serão checadas com ACL posteriormente, desabilite se isso é um problema de segurança para você.';
+$lang['template']              = 'Qual predefinição deveria ser usada para formatar os PDFs?';
+$lang['maxbookmarks']          = 'Quantos níveis de seções deveriam ser usados nos marcadores PDF? <small>(0=none, 5=all)</small>';
+$lang['usestyles']             = 'Você pode gerar uma lista de plugins separadas por vírgula nos quais <code>style.css</code> ou <code>screen.css</code> deveriam ser usadas para a gerar um PDF.';
+$lang['qrcodesize']            = 'Tamanho do QR code incorporado (em pixels <code><i>largura</i><b>x</b><i>altura</i></code>). Vazio para desabilitar';
