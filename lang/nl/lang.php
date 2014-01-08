09974b1335ce7c33a4401be02c59523ef593689e
Gerrit Uitslag <klapinklapin@gmail.com>
2014-01-08 10:10:34 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['export_pdf_button']     = 'Exporteren naar PDF';
