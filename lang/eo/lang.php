07dd0c0d5adb1e9ac9d767b2abfbdde33d1be5a7
Robert Bogenschneider <bogi@uea.org>
2014-02-08 23:00:05 +0100

    translation update

---
+++ lang/eo/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['export_pdf_button']     = 'Eksporti al PDF';
