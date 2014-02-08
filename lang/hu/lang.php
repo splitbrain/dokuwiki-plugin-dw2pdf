9333ac3c9ea241f99a12eb1e034ee3798b6c7cfd
Marina Vladi <deldadam@gmail.com>
2014-02-08 15:15:36 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['export_pdf_button']     = 'Exportálás PDF-be';
