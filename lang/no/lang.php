5179bbde9fb906a839c025f10a540b4f9c1119c4
Christopher Schive <chschive@frisurf.no>
2014-03-18 14:00:32 +0100

    translation update

---
+++ lang/no/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Christopher Schive <chschive@frisurf.no>
+ */
+$lang['export_pdf_button']     = 'Eksporter til PDF';
