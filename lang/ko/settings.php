3abcf212ebc6efaaca1083de2ae1d83e7fa6e618
Myeongjin <aranet100@gmail.com>
2014-04-23 13:05:06 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -13,3 +13,4 @@ $lang['template']              = '어떤 템플릿을 PDF 파일의 형식에 �
 $lang['maxbookmarks']          = '얼마나 많은 문단 수준을 PDF 책갈피에 사용되어야 합니까? <small>(0=없음, 5=모두)</small>';
 $lang['usestyles']             = 'PDF 생성에 사용되어야 하는 어느 <code>style.css</code>나 <code>screen.css</code>의 플러그인의 쉼표로 구분된 목록을 제공할 수 있습니다. 기본적으로 <code>print.css</code>와 <code>pdf.css</code>만 사용됩니다.';
 $lang['qrcodesize']            = '포함된 QR 코드의 크기. (픽셀로 <code><i>너비</i><b>x</b><i>높이</i></code>) 비활성화하려면 비우세요';
+$lang['showexportbutton']      = 'PDF 내보내기 버튼 보이기 (템플릿이 지원하고, 템플릿이 허용할 때만)';
