e3ccbbf17fab737d9242fe7ec726bf8cd652c61e
Davor Turkalj <turki.bsc@gmail.com>
2014-07-10 15:15:27 +0200

    translation update

---
+++ lang/hr/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['output']                = 'Kako da bude PDF prikazan korisniku';
+$lang['output_o_browser']      = 'Prikaži u pregledniku';
+$lang['output_o_file']         = 'Snimi PDF u datoteku';
+$lang['pagesize']              = 'Format stranice kako je podržan u mPDF. Uobičajeno <code>A4</code> ili <code>letter</code>.';
+$lang['orientation']           = 'Orijentacija stranice';
+$lang['orientation_o_portrait'] = 'Portret';
+$lang['orientation_o_landscape'] = 'Pejzaž';
+$lang['usecache']              = 'Da li će PDF-ovi bit priručno pohranjeni ? Tada neće biti provjeravana autorizacija na ubačene slike. Onemogućite ako je to sigurnosni problem.';
+$lang['template']              = 'Koji predložak da koristim za oblikovanje PDF-a?';
+$lang['maxbookmarks']          = 'Koliko nivoa da se koristi za PDF oznake? <small>(0=ništa, 5=sve)</small>';
+$lang['usestyles']             = 'Navedite zarezom odvojenu listu dodataka čiji <code>style.css</code> ili <code>screen.css</code> će biti korišteni pri kreiranju PDF-a. Podrazumjevano se koriste samo <code>print.css</code> i <code>pdf.css</code>.';
+$lang['qrcodesize']            = 'Veličina ugrađenog QR koda (u pikselima <code><i>širina</i><b>x</b><i>visina</i></code>). Prazno za onemogućavanje';
+$lang['showexportbutton']      = 'Prikaži Ispis u PDF dugme (samo ako je podržano od strane DokuWiki predloška)';
