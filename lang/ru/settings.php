5588fd66217b4d5abeb40ae9d7ebf2253e89a53a
Vasilyy Balyasnyy <v.balyasnyy@gmail.com>
2014-10-01 10:00:07 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Vasilyy Balyasnyy <v.balyasnyy@gmail.com>
+ */
+$lang['output']                = 'Как должен PDF быть предоставлен пользователю?';
+$lang['output_o_browser']      = 'Показать в браузере';
+$lang['output_o_file']         = 'Скачать PDF';
+$lang['pagesize']              = 'Формат страницы поддерживаемый mPDF. Обычно <code>A4</code> или <code>letter</code>.';
+$lang['orientation']           = 'Ориентация страницы.';
+$lang['orientation_o_portrait'] = 'Вертикально';
+$lang['orientation_o_landscape'] = 'Горизонтально';
+$lang['usecache']              = 'Кэшировать PDF? Встроенные картинки могут быть не проверены ACL, отключить, если это не безопасно для вас.';
+$lang['template']              = 'Какой шаблон должен быть выбран для форматирования PDF?';
+$lang['maxbookmarks']          = 'Сколько уровней вкладок должно быть использовано для закладок PDF? <small>(0=none, 5=all)</small>';
+$lang['usestyles']             = 'Вы можете указать разделенный запятыми список плагинов которые <code>style.css</code> или <code>screen.css</code> будут использованы для генерации PDF. По умолчанию только <code>print.css</code> и <code>pdf.css</code> используются.';
+$lang['qrcodesize']            = 'Размер встроенного QR кода (в пикселях <code><i>ширина</i><b>x</b><i>высота</i></code>). Пропустить, если хотите отключить.';
+$lang['showexportbutton']      = 'Показать кнопку экспорта PDF (только если поддерживается вашим плагином)';
