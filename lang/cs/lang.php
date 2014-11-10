7246765fc5163fd7cdb920f4e1c6082245a26edf
Marek Ištvánek <marek.istvanek@gmail.com>
2014-11-10 12:44:03 +0100

    Partial czech translation

---
+++ lang/cs/lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['export_pdf_button'] = "Uložit do PDF";
+$lang['needtitle']         = "Zadejte prosím název.";
+$lang['needns']            = "Zadejte prosím existující jmenný prostor.";
+$lang['empty']             = "Zatím nejsou vybrané stránky.";
