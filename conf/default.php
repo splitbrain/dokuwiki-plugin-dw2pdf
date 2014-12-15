dbd8d9a179bb15614ece8e7b8e6aee8d87d4e349
Gerrit Uitslag <klapinklapin@gmail.com>
2014-12-15 16:44:40 +0100

    group overrideable settings in plugin config

--- conf/default.php
+++ conf/default.php
@@ -1,12 +1,12 @@
 <?php
-$conf['output']           = 'file';
 $conf['pagesize']         = 'A4';
 $conf['orientation']      = 'portrait';
 $conf['toc']              = 0;
 $conf['toclevels']        = '';
 $conf['maxbookmarks']     = 5;
-$conf['usecache']         = 1;
 $conf['template']         = 'default';
+$conf['output']           = 'file';
+$conf['usecache']         = 1;
 $conf['usestyles']        = '';
 $conf['qrcodesize']       = '120x120';
 $conf['showexportbutton'] = 1;
--- conf/metadata.php
+++ conf/metadata.php
@@ -1,12 +1,12 @@
 <?php
-$meta['output']           = array('multichoice', '_choices' => array('browser', 'file'));
 $meta['pagesize']         = array('string');
 $meta['orientation']      = array('multichoice', '_choices' => array('portrait', 'landscape'));
 $meta['toc']              = array('onoff');
 $meta['toclevels']        = array('string', '_pattern' => '/^(|[1-5]-[1-5])$/');
 $meta['maxbookmarks']     = array('numeric');
-$meta['usecache']         = array('onoff');
 $meta['template']         = array('dirchoice', '_dir' => DOKU_PLUGIN . 'dw2pdf/tpl/');
+$meta['output']           = array('multichoice', '_choices' => array('browser', 'file'));
+$meta['usecache']         = array('onoff');
 $meta['usestyles']        = array('string');
 $meta['qrcodesize']       = array('string', '_pattern' => '/^(|\d+x\d+)$/');
 $meta['showexportbutton'] = array('onoff');
