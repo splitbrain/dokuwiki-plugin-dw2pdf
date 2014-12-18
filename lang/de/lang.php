12b0bbd41ae3bd13a3c703dff8d6c4eaaf94903b
Juergen-aus-Koeln <h-j-schuemmer@web.de>
2014-12-18 15:15:38 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,5 +1,12 @@
 <?php
 
-$lang['export_pdf_button'] = "PDF exportieren";
-$lang['empty']                 = 'Sie haben noch keine Seiten gewählt.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Juergen-aus-Koeln <h-j-schuemmer@web.de>
+ */
+$lang['export_pdf_button']     = 'PDF exportieren';
 $lang['needtitle']             = 'Bitte Titel angeben!';
+$lang['needns']                = 'Bitte geben Sie einen vorhandenen Namensraum an.';
+$lang['empty']                 = 'Sie haben noch keine Seiten gewählt.';
+$lang['tocheader']             = 'Inhaltsverzeichnis';
