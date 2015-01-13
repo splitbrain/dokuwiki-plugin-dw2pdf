fb79c28e900aeaeffac0f459189e92c8b325e315
Esteban Monge <emonge@emonge.com>
2015-01-13 13:45:55 -0600

    Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['export_pdf_button'] = "Exportar a PDF";
+$lang['needtitle']         = "Por favor proporcione un título.";
+$lang['needns']            = "Por favor proporcione un nombre de espacio existente.";
+$lang['empty']             = "No tiene páginas seleccionadas aún.";
+$lang['tocheader']         = "Tabla de Contenidos";
