be84c7f0b59ee3a8908b3e78dd5ebce4c0a210a2
Robert Bogenschneider <robog@gmx.de>
2015-04-23 08:05:17 +0200

    translation update

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -4,7 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Robert Bogenschneider <bogi@uea.org>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
 $lang['export_pdf_button']     = 'Eksporti al PDF';
+$lang['needtitle']             = 'Bonvolu indiki titolon.';
+$lang['needns']                = 'Bv. indiki ekzistantan nomspacon.';
 $lang['empty']                 = 'Vi ankoraŭ ne selektis paĝojn.';
-$lang['needtitle']             = 'Bonvolu indiki titolon';
+$lang['tocheader']             = 'Enhavtabelo';
