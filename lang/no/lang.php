07144d48d8b641075c1c15e718205147a8900cd9
Daniel Raknes <rada@jbv.no>
2015-06-29 11:40:14 +0200

    translation update

--- lang/no/lang.php
+++ lang/no/lang.php
@@ -4,5 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Christopher Schive <chschive@frisurf.no>
+ * @author Daniel Raknes <rada@jbv.no>
  */
 $lang['export_pdf_button']     = 'Eksporter til PDF';
+$lang['needtitle']             = 'Legg inn tittel.';
+$lang['needns']                = 'Legg inn eksisterende navnerom.';
+$lang['empty']                 = 'Du har ikke valgt noen sider ennå.';
+$lang['tocheader']             = 'Innholdsfortegnelse';
+$lang['export_ns']             = 'Eksporter navnerom "%s:" til filen %s.pdf';
