528811ffa03ff116f2f7947fd233ffa591bcaeff
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2016-02-04 17:26:51 +0100

    Adjust method signatures to match parent

--- renderer.php
+++ renderer.php
@@ -104,7 +104,7 @@ class renderer_plugin_dw2pdf extends Doku_Renderer_xhtml {
      * // modified copy of parent function
      * @see Doku_Renderer_xhtml::locallink
      */
-    function locallink($hash, $name = null) {
+    function locallink($hash, $name = null, $returnonly = false) {
         global $ID;
         $name  = $this->_getLinkTitle($name, $hash, $isImage);
         $hash  = $this->_headerToLink($hash);
