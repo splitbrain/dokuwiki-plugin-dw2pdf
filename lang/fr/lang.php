7af141d91ebc4790ec68cc8fba90da485eeebadc
Schplurtz le Déboulonné <Schlurtz@laposte.net>
2017-04-04 18:35:16 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,11 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author tuxun <tuxuntrash@gmail.com>
- * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ * @author Schplurtz le Déboulonné <Schlurtz@laposte.net>
  */
 $lang['export_pdf_button']     = 'Exporter en PDF';
 $lang['needtitle']             = 'Veuillez choisir un titre.';
@@ -14,3 +14,4 @@ $lang['needns']                = 'Veuillez choisir une catégorie existante.';
 $lang['empty']                 = 'Vous n\'avez pas encore sélectionné de pages.';
 $lang['tocheader']             = 'Table des matières';
 $lang['export_ns']             = 'Exporter la catégorie  « %s » dans le fichier %s.pdf.';
+$lang['forbidden']             = 'Vous n\'avez pas accès à ces pages : %s<br/><br/>Utilisez l\'option "Sautez les pages interdites" pour créer votre livre avec les pages disponibles.';
