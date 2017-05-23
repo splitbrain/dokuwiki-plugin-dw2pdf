04533720e9fce9b42c412f16e6391de97f5038b1
Davor Turkalj <turki.bsc@gmail.com>
2017-05-23 15:40:05 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
 $lang['export_pdf_button']     = 'Izvoz u PDF';
@@ -11,3 +11,4 @@ $lang['needns']                = 'Molim navedite postojeći imenski prostor.';
 $lang['empty']                 = 'Nemate još odabranih stranica.';
 $lang['tocheader']             = 'Sadržaj';
 $lang['export_ns']             = 'Izvoz imenskog prostora "%s:" u %s.pdf';
+$lang['forbidden']             = 'Nemate pristup ovim stranicama: %s.<br/><br/>Koristite opciju \'Skip Forbidden Pages\' da kreirate knjigu s raspoloživim stranicama.';
--- lang/hr/settings.php
+++ lang/hr/settings.php
@@ -2,13 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
 $lang['pagesize']              = 'Format stranice kako je podržan u mPDF. Uobičajeno <code>A4</code> ili <code>letter</code>.';
 $lang['orientation']           = 'Orijentacija stranice';
 $lang['orientation_o_portrait'] = 'Portret';
 $lang['orientation_o_landscape'] = 'Pejzaž';
+$lang['font-size']             = 'Veličina fonta za normalni tekst u točkama.';
 $lang['doublesided']           = 'Dvostrani dokumenti počinju na neparnoj stranici i imaju parove parnih i neparnih stranica. Jednostrani dokumenti imaju samo parne stranice.';
 $lang['toc']                   = 'Dodaj automatski generirani sadržaj u PDF (Napomena: prazne stranice mogu biti dodane na parne stranice, a sadržaj je uvijek uključen na neparnim brojevima stranica, stranice Sadržaja nemaju brojeve stranica)';
 $lang['toclevels']             = 'Podesite najviši nivo i maksimalan broj nivoa koji će biti uključeni u sadržaj. Podrazumijevani wiki nivoi sadržaja su <a href="#config___toptoclevel">toptoclevel</a> i <a href="#config___maxtoclevel">maxtoclevel</a>. Format: <code><i>&lt;top&gt;</i>-<i>&lt;max&gt;</i></code>';
