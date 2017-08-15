026b594bb55d8ddc29ab361fa1990225c9debf6c
Andreas Gohr <andi@splitbrain.org>
2017-08-15 21:45:22 +0200

    implement the PDF export button for the new menu system
    This works with the changes proposed in splitbrain/dokuwiki#1821

---
+++ MenuItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace dokuwiki\plugin\dw2pdf;
+
+use dokuwiki\Menu\Item\AbstractItem;
+
+/**
+ * Class MenuItem
+ *
+ * Implements the PDF export button for DokuWiki's menu system
+ *
+ * @package dokuwiki\plugin\dw2pdf
+ */
+class MenuItem extends AbstractItem {
+
+    /** @var string do action for this plugin */
+    protected $type = 'export_pdf';
+
+    /** @var string icon file */
+    protected $svg = __DIR__ . '/file-pdf.svg';
+
+    /**
+     * MenuItem constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+        global $REV;
+        if($REV) $this->params['rev'] = $REV;
+    }
+
+    /**
+     * Get label from plugin language file
+     *
+     * @return string
+     */
+    public function getLabel() {
+        $hlp = plugin_load('action', 'dw2pdf');
+        return $hlp->getLang('export_pdf_button');
+    }
+}
