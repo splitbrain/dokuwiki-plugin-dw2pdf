df2d281670aa0eee428f0a0eba33015baf9ca01d
Марко М. Костић <marko.m.kostic@gmail.com>
2017-08-29 05:15:14 +0200

    translation update

---
+++ lang/sr/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['orientation']           = 'Усмерење странице.';
+$lang['orientation_o_portrait'] = 'Усправно';
+$lang['orientation_o_landscape'] = 'Положено';
+$lang['output_o_browser']      = 'Прикажи у прегледачу';
+$lang['output_o_file']         = 'Преузми ПДФ';
