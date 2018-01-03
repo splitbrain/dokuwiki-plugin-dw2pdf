01d8b24a2d02f54e2b5a243b2b538978d17b32ba
mark prins <mprins@users.sf.net>
2018-01-03 12:25:11 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,14 +3,15 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author mark prins <mprins@users.sf.net>
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Wouter Wijsman <wwijsman@live.nl>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
- * @author mark prins <mprins@users.sf.net>
  */
 $lang['export_pdf_button']     = 'Exporteren naar PDF';
 $lang['needtitle']             = 'Er moet een titel ingevuld worden';
-$lang['needns']                = 'Geef een bestaande namespace.';
+$lang['needns']                = 'Geef een bestaande naamruimte.';
+$lang['notexist']              = 'Gevraagde pagina bestaat niet';
 $lang['empty']                 = 'Er zijn nog geen pagina\'s geselecteerd.';
 $lang['tocheader']             = 'Inhoudsopgave';
 $lang['export_ns']             = 'Exporteren namespace "%s:" naar bestand %s.pdf';
