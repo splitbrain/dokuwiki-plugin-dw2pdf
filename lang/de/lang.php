a7922d032219102b786ac11f636f95e6f52f7347
Sebastian Engel <mail@engel-s.de>
2018-01-05 10:15:12 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,12 +3,14 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Sebastian Engel <mail@engel-s.de>
  * @author Juergen-aus-Koeln <h-j-schuemmer@web.de>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
 $lang['export_pdf_button']     = 'PDF exportieren';
 $lang['needtitle']             = 'Bitte Titel angeben!';
 $lang['needns']                = 'Bitte geben Sie einen vorhandenen Namensraum an.';
+$lang['notexist']              = 'Die angeforderte Seite existiert nicht.';
 $lang['empty']                 = 'Sie haben noch keine Seiten gewählt.';
 $lang['tocheader']             = 'Inhaltsverzeichnis';
 $lang['export_ns']             = 'Exportiere Namensraum "%s:" in Datei %s.pdf';
