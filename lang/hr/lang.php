f0f0e1f39daa7ca1a09f0394ff18d1d176fadbc4
Davor Turkalj <turki.bsc@gmail.com>
2018-01-18 23:45:34 +0100

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -6,8 +6,9 @@
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
 $lang['export_pdf_button']     = 'Izvoz u PDF';
-$lang['needtitle']             = 'Molim unesite naslov';
+$lang['needtitle']             = 'Molim unesite naslov.';
 $lang['needns']                = 'Molim navedite postojeći imenski prostor.';
+$lang['notexist']              = 'Tražena stranica ne postoji.';
 $lang['empty']                 = 'Nemate još odabranih stranica.';
 $lang['tocheader']             = 'Sadržaj';
 $lang['export_ns']             = 'Izvoz imenskog prostora "%s:" u %s.pdf';
