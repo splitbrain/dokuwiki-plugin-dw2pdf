ea5a72cfbea986fa398a014869704bfb049e9cb1
qezwan <qezwan@gmail.com>
2020-10-06 11:25:05 +0200

    translation update

---
+++ lang/ckb/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['pagesize']              = 'فۆرماتی لاپەڕە وەک لەلایەن mPDF پشتگیری کراوە. هەمیشە <code> ئەی 4 </code> یان <code> پیتی </code>.';
+$lang['orientation']           = 'ئاراستەی لاپەڕە.';
+$lang['orientation_o_portrait'] = 'درێژاو';
+$lang['orientation_o_landscape'] = 'پانایی';
+$lang['font-size']             = 'قەبارەی فۆنت بۆ دەقی ئاسایی لە خاڵەکان.';
+$lang['doublesided']           = 'دۆکیومێنتی جووت لا دەست دەکات بە زیادکردنی لاپەڕەی تاک، و جووتی لاپەڕەی جووت و تاک. بەڵگەنامەی تاک-لا تەنها پەڕەی تاک و تاکیان هەیە.';
+$lang['toc']                   = 'زیادکردنی خشتەی دروستکراوی خۆکار ناوەڕۆک لە PDF (تێبینی: ئایا دەکرێت پەڕەی بەتاڵ زیاد بکەیت بەهۆی دەستپێکردن لە لاپەڕەیەکی تاک و ToC هەمیشە ژمارەی لاپەڕە جووتەکان لەخۆ دەگرێت، لاپەڕەکانی ToC خۆی هیچ ژمارەی لاپەڕەینیە)';
+$lang['toclevels']             = 'پێناسەی ئاستی سەرەوە و بەرزترین ئاستی قوڵایی کە زیادکراون بۆ ToC. ئاستەکانی گریمانەیی ویکی بۆ C <a href="#config___toptoclevel">toptoclevel</a> و <a href="#config___maxtoclevel">maxtoclevel </a> بەکاردێت. فۆرماتی: <code><i>&lt;top&gt;</i>-<i>&lt;max&gt;</i></code>';
+$lang['maxbookmarks']          = 'دەبێت چەند ئاستی بەش لە نیشاندەری PDF بەکار بهێنرێت؟ <small>(0=هیچ، 5=هەموو)</small>';
+$lang['template']              = 'کام ڕووکار دەبێت بەکار بێت بۆ فۆرماتی PDFs?';
+$lang['output']                = 'PDF چۆن پێشکەش بە بەکارهێنەر بکرێت؟';
+$lang['output_o_browser']      = 'لە وێبگەڕەکە دا پیشان بدە';
+$lang['output_o_file']         = 'داگرتنی PDF';
+$lang['usecache']              = 'ئایا دەبێت PDFs خەزن بکرێت؟ وێنە بنچینکراوەکان ئەوسا ناپشکنرێن ACL ئەوسا، ناچالاک دەکرێت ئەگەر ئەمە نیگەرانی ئاسایشبێت بۆ تۆ.';
+$lang['usestyles']             = 'دەتوانیت لیستێکی جیاکراوەی کۆما ی پێوەکراو بەو پێوەکراوانە ببەخشیت کە <code>style.css</code> یان <code>screen.css</code> پێویستە بۆ نەوەی PDF بەکار بهێنرێت. بە گریمانەیی تەنها <code>print.css</code> و <code>pdf.css </code> بەکاردەهێندرێن.';
+$lang['qrcodesize']            = 'قەبارەی کۆدی QR ی بنچینەکراو (بە <code><i>&lt;پانی&gt;</i><b>x</b><i>&lt;height&gt;</i></code>). بەتاڵ بۆ لەکارخستنی';
+$lang['showexportbutton']      = 'دوگمەی هەناردەی PDF نیشان بدە (تەنها کاتێک کە لەلایەن تێمپلەیتەکەتەوە پشتگیری دەکرێت)';
