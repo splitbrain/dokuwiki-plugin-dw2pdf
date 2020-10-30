54d1c260d85a490d2c421ccdbf8ab134c611a20c
Alex <write-tome@mail.ru>
2020-10-30 08:58:13 +0500

    Update P.php
    A workaround for issue #275

--- vendor/mpdf/mpdf/src/Tag/P.php
+++ vendor/mpdf/mpdf/src/Tag/P.php
@@ -1,9 +1,23 @@
 <?php
-
+ 
 namespace Mpdf\Tag;
+ 
+class P extends InlineTag
+{	
+ 
+	public function open($attr, &$ahtml, &$ihtml)
+	{	
 
-class P extends BlockTag
-{
+		parent::open($attr, $ahtml, $ihtml);
+		
+	}
+ 
+	public function close(&$ahtml, &$ihtml)
+	{
 
+		parent::close($ahtml, $ihtml);
 
+		$this->mpdf->_saveTextBuffer("\n");
+		$this->mpdf->_saveTextBuffer("\n");
+		
 }
