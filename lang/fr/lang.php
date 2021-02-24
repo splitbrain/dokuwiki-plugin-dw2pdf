afd92f7bbe62396a59924bb188655edcfe44b897
Hérisson grognon <dodoperso@laposte.net>
2021-02-24 14:20:33 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hérisson grognon <dodoperso@laposte.net>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author tuxun <tuxuntrash@gmail.com>
@@ -11,8 +12,8 @@
 $lang['export_pdf_button']     = 'Exporter en PDF';
 $lang['needtitle']             = 'Veuillez choisir un titre.';
 $lang['needns']                = 'Veuillez choisir une catégorie existante.';
-$lang['notexist']              = 'La page demandée n\'existe pas';
-$lang['empty']                 = 'Vous n\'avez pas encore sélectionné de pages.';
+$lang['notexist']              = 'La page demandée n\'existe pas.';
+$lang['empty']                 = 'Vous n\'avez encore sélectionné aucune page.';
 $lang['tocheader']             = 'Table des matières';
 $lang['export_ns']             = 'Exporter la catégorie  « %s » dans le fichier %s.pdf.';
 $lang['forbidden']             = 'Vous n\'avez pas accès à ces pages : %s.<br/><br/>Utilisez l\'option \'Sauter les pages interdites\' pour créer votre livre avec les pages disponibles.';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hérisson grognon <dodoperso@laposte.net>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author NicolasFriedli <nicolas@theologique.ch>
  * @author Fabrice Dejaigher <fabrice@chtiland.com>
@@ -19,11 +20,11 @@ $lang['toc']                   = 'Ajouter au PDF une table des matières génér
 $lang['toclevels']             = 'Définit le plus haut niveau et la profondeur maximum des titres ajoutés à la TdM. Par défaut, les niveaux de la TdM du wiki <a href="#config___toptoclevel"><i>toptoclevel</i></a> et <a href="#config___maxtoclevel"><i>maxtocleve</i>l</a> sont utilisés.<br />Format&nbsp;: <code><i>&lt;niveau_haut&gt;</i>-<i>&lt;niveau_max&gt;</i></code>';
 $lang['maxbookmarks']          = 'Combien de niveaux de section (titres) doivent être utilisés dans les marque-pages PDF ?
 <small>(0=aucun, 5=tous)</small>';
-$lang['template']              = 'Quel thème doit être utilisé pour présenter les PDF?';
-$lang['output']                = 'Comment le PDF doit-il être présenté à l\'utilisateur?';
+$lang['template']              = 'Quel thème doit être utilisé pour présenter les PDF ?';
+$lang['output']                = 'Comment le PDF doit-il être présenté à l\'utilisateur ?';
 $lang['output_o_browser']      = 'Afficher dans le navigateur';
 $lang['output_o_file']         = 'Télécharger le PDF';
 $lang['usecache']              = 'Mettre les PDF en cache ? Les images incluses le seront, alors, sans vérification des droits (ACL), désactivez cette option si cela vous pose un problème de sécurité.';
 $lang['usestyles']             = 'Vous pouvez préciser une liste d\'extensions dont les fichiers <code>style.css</code> ou <code>screen.css</code> doivent être utilisés pour générer les PDF. Par défaut,  seuls les fichiers  <code>print.css</code> et <code>pdf.css</code> sont utilisés.';
 $lang['qrcodesize']            = 'Taille du code QR  (en pixels <code><i>largeur</i><b>x</b><i>hauteur</i></code>). Laisser vide pour le désactiver.';
-$lang['showexportbutton']      = 'Afficher le bouton «Exporter en PDF». (seulement pour les thèmes validés et prenant en charge cette fonctionnalité)';
+$lang['showexportbutton']      = 'Afficher le bouton « Exporter en PDF » (seulement pour les thèmes validés et prenant en charge cette fonctionnalité)';
