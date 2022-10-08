e50c39cc5175472f6779f1933c87d358dd5a5b56
Gerrit Uitslag <klapinklapin@gmail.com>
2022-10-08 23:58:37 +0200

    Update syntax_exportlink.test.php

--- _test/syntax_exportlink.test.php
+++ _test/syntax_exportlink.test.php
@@ -35,7 +35,7 @@ class dw2pdf_syntax_exportlink_test extends DokuWikiTest {
         );
         $this->assertEquals($expected_parser_response,$parser_response[2]);
         $renderer_response = p_render('xhtml',$parser_response,$info);
-        $expected_renderer_response = 'doku.php?id=foo:bar:start&amp;do=export_pdfns&amp;book_ns=foo:bar&amp;book_title=Foo" class="wikilink2" title="foo:bar:start" rel="nofollow" data-wiki-id="foo:bar:start">Export namespace &quot;foo:bar:&quot; to file Foo.pdf</a>';
+        $expected_renderer_response = 'doku.php?id=foo:bar:start&amp;do=export_pdfns&amp;book_ns=foo:bar&amp;book_title=Foo" class="wikilink2" title="foo:bar:start" rel="nofollow">Export namespace &quot;foo:bar:&quot; to file Foo.pdf</a>';
         $trimmed_renderer_response = substr($renderer_response,strpos($renderer_response,'doku.php'),-5);
         $trimmed_renderer_response = trim($trimmed_renderer_response);
         $this->assertEquals($expected_renderer_response,$trimmed_renderer_response);
