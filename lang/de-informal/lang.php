34a99c0881cc10379216fdf173a7c49eac40309e
Hella <hella.breitkopf@gmail.com>
2023-01-04 10:40:08 +0100

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author DrMaXX <drmaxxis@gmail.com>
  * @author Sebastian Engel <mail@engel-s.de>
  * @author Thomas Templin <templin@gnuwhv.de>
@@ -12,8 +13,8 @@ $lang['export_pdf_button']     = 'PDF exportieren';
 $lang['needtitle']             = 'Bitte Titel angeben!';
 $lang['needns']                = 'Bitte gib einen vorhandenen Namensraum an.';
 $lang['notexist']              = 'Die angeforderte Seite existiert nicht.';
-$lang['empty']                 = 'Du hastnoch keine Seiten gewählt.';
+$lang['empty']                 = 'Du hast noch keine Seiten gewählt.';
 $lang['tocheader']             = 'Inhaltsverzeichnis';
 $lang['export_ns']             = 'Exportiere Namensraum "%s:" in Datei %s.pdf';
-$lang['forbidden']             = 'Sie haben keine Berechtigung für folgende Seiten: %s.<br/><br/>Benutze die Option \'Seiten ohne Berechtigung auslassen\' um das Buch mit den verfügbaren Seiten zu erstellen.';
+$lang['forbidden']             = 'Du hast keine Berechtigung für folgende Seiten: %s.<br/><br/>Benutze die Option \'Seiten ohne Berechtigung auslassen\' um das Buch mit den verfügbaren Seiten zu erstellen.';
 $lang['missingbookcreator']    = 'Das Bookcreator Plugin ist nicht installiert oder deaktiviert';
