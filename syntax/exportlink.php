1d0088a03a51fcb604df2c443c7f90a92fe6d802
Gerrit Uitslag <klapinklapin@gmail.com>
2023-07-29 16:23:34 +0200

    cleanup/refactor exportlink

--- syntax/exportlink.php
+++ syntax/exportlink.php
@@ -7,13 +7,14 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Sam Wilson <sam@samwilson.id.au>
  */
-/* Must be run within Dokuwiki */
-if(!defined('DOKU_INC')) die();
+
+use dokuwiki\File\PageResolver;
 
 /**
  * Syntax for page specific directions for mpdf library
  */
-class syntax_plugin_dw2pdf_exportlink extends DokuWiki_Syntax_Plugin {
+class syntax_plugin_dw2pdf_exportlink extends DokuWiki_Syntax_Plugin
+{
 
     /**
      * Syntax Type
@@ -22,7 +23,8 @@ class syntax_plugin_dw2pdf_exportlink extends DokuWiki_Syntax_Plugin {
      *
      * @return string
      */
-    public function getType() {
+    public function getType()
+    {
         return 'substition';
     }
 
@@ -31,56 +33,65 @@ class syntax_plugin_dw2pdf_exportlink extends DokuWiki_Syntax_Plugin {
      *
      * @return int
      */
-    public function getSort() {
+    public function getSort()
+    {
         return 41;
     }
 
     /**
      * @param string $mode
      */
-    public function connectTo($mode) {
+    public function connectTo($mode)
+    {
         $this->Lexer->addSpecialPattern('~~PDFNS>(?:.*?)\|(?:.*?)~~', $mode, 'plugin_dw2pdf_exportlink');
     }
 
     /**
      * Handler to prepare matched data for the rendering process
      *
-     * @param   string       $match   The text matched by the patterns
-     * @param   int          $state   The lexer state for the match
-     * @param   int          $pos     The character position of the matched text
-     * @param   Doku_Handler $handler The Doku_Handler object
-     * @return  bool|array Return an array with all data you want to use in render, false don't add an instruction
+     * @param string $match The text matched by the patterns
+     * @param int $state The lexer state for the match
+     * @param int $pos The character position of the matched text
+     * @param Doku_Handler $handler The Doku_Handler object
+     * @return  array Return an array with all data you want to use in render, false don't add an instruction
      */
-    public function handle($match, $state, $pos, Doku_Handler $handler) {
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
         global $ID;
-        $ns = substr($match,8,strpos($match,'|')-8);
+        $ns = substr($match, 8, strpos($match, '|') - 8);
         $id = $ns . ':start';
-        $title = substr($match,strpos($match,'|')+1,-2);
         $resolver = new PageResolver($ID);
         $page = $resolver->resolveId($id);
         $ns = getNS($page);
+        $title = substr($match, strpos($match, '|') + 1, -2);
         $link = '?do=export_pdfns&book_ns=' . $ns . '&book_title=' . $title;
 
         // check if there is an ampersand in the title
-        $amp = strpos($title,'&');
+        $amp = strpos($title, '&');
         if ($amp !== false) {
-            $title = substr($title,0,$amp);
+            $title = substr($title, 0, $amp);
         }
 
-        return array('link' => $link, 'title' => sprintf($this->getLang('export_ns'),$ns,$title),$state, $pos);
+        return [
+            'link' => $link,
+            'title' => sprintf($this->getLang('export_ns'), $ns, $title),
+            $state,
+            $pos
+        ];
     }
 
     /**
      * Handles the actual output creation.
      *
-     * @param string        $mode     output format being rendered
+     * @param string $format output format being rendered
      * @param Doku_Renderer $renderer the current renderer object
-     * @param array         $data     data created by handler()
+     * @param array $data data created by handler()
      * @return  boolean                 rendered correctly? (however, returned value is not used at the moment)
      */
-    public function render($mode, Doku_Renderer $renderer, $data) {
-        if($mode == 'xhtml' && !is_a($renderer,'renderer_plugin_dw2pdf')) {
-            $renderer->internallink($data['link'],$data['title']);
+    public function render($format, Doku_Renderer $renderer, $data)
+    {
+        if ($format == 'xhtml' && !is_a($renderer, 'renderer_plugin_dw2pdf')) {
+            $renderer->internallink($data['link'], $data['title']);
             return true;
         }
         return false;
