7dc19e5d6625d9c3fe3da24995250b5b98b61887
Otto Riehl <post@otto-riehl.de>
2023-09-11 13:25:32 +0200

    translation update

---
+++ lang/uk/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Otto Riehl <post@otto-riehl.de>
+ */
+$lang['export_pdf_button']     = 'Експорт до PDF';
+$lang['needtitle']             = 'Будь ласка, вкажіть назву.';
+$lang['needns']                = 'Будь ласка, вкажіть існуючий простір імен.';
+$lang['notexist']              = 'Запитувана сторінка не існує.';
+$lang['empty']                 = 'Ви ще не вибрали жодної сторінки.';
+$lang['tocheader']             = 'Зміст';
+$lang['export_ns']             = 'Експортувати простір імен "%s:" до файлу %s.pdf';
+$lang['forbidden']             = 'Ви не маєте доступу до цих сторінок: %s.<br/><br/>Використайте опцію \'Пропустити заборонені сторінки\', щоб створити книгу з доступними сторінками.';
+$lang['missingbookcreator']    = 'Плагін Bookcreator не встановлено або вимкнено';
